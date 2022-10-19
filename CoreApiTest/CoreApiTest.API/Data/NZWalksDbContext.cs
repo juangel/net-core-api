1c0b9596c4504f2f8aa172503ddc318f7b107da7
jaguirre <ing.jaguirre@hotmail.com>
2022-10-19 17:09:31 -0500

    End of Section 2

---
+++ CoreApiTest/CoreApiTest.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CoreApiTest.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreApiTest.API.Data
+{
+    public class NZWalksDbContext:DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
---
+++ CoreApiTest/CoreApiTest.API/Migrations/20221019220704_InitialFix.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoreApiTest.API.Migrations
+{
+    public partial class InitialFix : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Walks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "Walks",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
