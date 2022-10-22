73b4ff98a8f8f61fe058309244435affd0fe749b
jaguirre <ing.jaguirre@hotmail.com>
2022-10-22 03:39:34 -0500

    End of Section 5

---
+++ CoreApiTest/CoreApiTest.API/Controllers/WalksController.cs
@@ -0,0 +1,114 @@
+﻿using AutoMapper;
+using CoreApiTest.API.Models.Domain;
+using CoreApiTest.API.Models.DTO;
+using CoreApiTest.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreApiTest.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalkRepository walkRepository, IMapper mapper)
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWaksAsync()
+        {
+            // Fetch data from database - domain walks
+            var walksDomain = await walkRepository.GetAllAsync();
+
+            // Convert domain walks to DTO walks
+            var walksDTO = mapper.Map<List<Models.DTO.Walk>>(walksDomain);
+
+            return Ok(walksDTO);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetWalkAsync")]
+        public async Task<IActionResult> GetWalkAsync(Guid id)
+        {
+            // Get walk domain object from database
+            var walkDomain = await walkRepository.GetAsync(id);
+
+            //Convert object to DTO
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walkDomain);
+
+            // Return response
+            return Ok(walkDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkAsync([FromBody] Models.DTO.AddWalkRequest addWalkRequest)
+        {
+            // Convert DTO to domain object
+            var walkDomain = new Models.Domain.Walk
+            {
+                Length = addWalkRequest.Length,
+                Name = addWalkRequest.Name,
+                RegionId = addWalkRequest.RegionId,
+                WalkDifficultyId = addWalkRequest.WalkDifficultyId
+            };
+
+            // Pass domain object to Repository to persist this
+            walkDomain = await walkRepository.AddAsync(walkDomain);
+
+            // Convert the Domain object back to DTO
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walkDomain);
+
+            // Send DTO response back to Client
+            return CreatedAtAction(nameof(GetWalkAsync), new { id = walkDTO.Id }, walkDTO);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateWalkAsync([FromRoute] Guid id, [FromBody] Models.DTO.UpdateWalkRequest updateWalkRequest)
+        {
+            // Convert DTO to domain object
+            var walkDomain = new Models.Domain.Walk
+            {
+                Length = updateWalkRequest.Length,
+                Name = updateWalkRequest.Name,
+                RegionId = updateWalkRequest.RegionId,
+                WalkDifficultyId = updateWalkRequest.WalkDifficultyId
+            };
+
+            // Pass details to repository - get domain object in response
+            walkDomain = await walkRepository.UpdateAsync(id, walkDomain);
+
+            // Handle null
+            if (walkDomain == null)
+                return NotFound();
+
+            // Convert back to DTO
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walkDomain);
+
+            // Return response
+            return Ok(walkDTO);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteWalkAsync(Guid id)
+        {
+            // call repository to delete walk
+            var walk = await walkRepository.DeleteAsync(id);
+
+            if (walk == null)
+                return NotFound();
+
+            // Convert response back to DTO
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+
+            return Ok(walkDTO);
+        }
+    }
+}
---
+++ CoreApiTest/CoreApiTest.API/Profiles/WalksProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace CoreApiTest.API.Profiles
+{
+    public class WalksProfile : Profile
+    {
+        public WalksProfile()
+        {
+            CreateMap<Models.Domain.Walk, Models.DTO.Walk>()
+                .ReverseMap();
+
+            CreateMap<Models.Domain.WalkDifficulty, Models.DTO.WalkDifficulty>()
+                .ReverseMap();
+
+        }
+    }
+}
